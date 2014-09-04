466da48e9191167425f6d2d5f299a3de6b86add0
Android55410113 <iiou_kill@hotmail.com>
2014-09-04 15:59:57 +0700

    ListView_55410113

---
+++ src/com/kantaporn/listview_55410113/list_items.java
@@ -0,0 +1,21 @@
+package com.kantaporn.listview_55410113;
+
+import android.graphics.drawable.Drawable;
+
+public class list_items {
+		private Drawable drawable;
+		private String title;
+		public Drawable getDrawable() {
+			return drawable;
+		}
+		public void setDrawable(Drawable drawable) {
+			this.drawable = drawable;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+}
